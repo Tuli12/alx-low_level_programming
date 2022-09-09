d1cd2d2df15c0f72f4bf0dfd1f65ba933908deb5
Tuli12 <Mariemutuli@gmail.com>
2022-09-08 18:38:42 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *	main - This uses program prints exactly the string
  *	"Programming is like building a multilingual puzzle
  *	followed by a new line.
- *	Return: always 0 (Success)     
- */
+ *	Return: always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
