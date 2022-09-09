393f4758e64c3af3ec6337430018377c9ff77f1b
Tuli12 <Mariemutuli@gmail.com>
2022-09-08 18:19:47 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 
 
-/*
- *  main - This uses program prints exactly the string
- *   "Programming is like building a multilingual puzzle
- *    followed by a new line.
- *     Return: always 0 (Success)     
-      */
+/**
+ *	 main - This uses program prints exactly the string
+ * 	"Programming is like building a multilingual puzzle
+ * 	 followed by a new line.
+ *	 Return: always 0 (Success)     
+ */
 
 int main(void)
 
