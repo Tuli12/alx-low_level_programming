96c5b92cb5ef9e2ad0c743f04fdf9557f39a5ffc
Tuli12 <Mariemutuli@gmail.com>
2022-09-14 18:26:54 -0700

    weeuuuh

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,16 @@
  * return (0)
  */
 
-void print_alphabet(void);
-{
+void print_alphabet(void)
+
+{	
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
+
 	{
 		_putchar(alpha);
-	{
-	_putchar('\n');
+	}
+
+		_putchar('\n');
 }
