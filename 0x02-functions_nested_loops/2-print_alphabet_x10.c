e08aab04f859a8fc7a76de9b4ac9f417914b3d0f
Tuli12 <Mariemutuli@gmail.com>
2022-09-15 06:48:25 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,18 @@
  * print_alphabet_x10 -function prints the alphabet x10
  * new line at end of code
  */
+
 void print_alphabet_x10(void)
 {
 	int alpha;
 	int alpha2;
 
-	for (alpha = 0; alpha <=9; alpha++)
+	for (alpha = 0; alpha <= 9; alpha++)
 	{
 		for (alpha2 = 'a' ; alpha2 <= 'z'; alpha2++)
-		{					
+		{		
 			_putchar(alpha2);
 		}
 		_putchar('\n');
-											}
+	}
 }
